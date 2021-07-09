23736bce936928f5a245cccf230f737a9f4ecdf7
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-09 11:39:13 +0300

    Create the model Profile

--- fitsgram/models.py
+++ fitsgram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.CloudinaryField('image',blank = True)
+    bio = models.TextField(max_length = 100,blank = True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    followers = models.ManyToManyField(User,blank=True,related_name='followers')
