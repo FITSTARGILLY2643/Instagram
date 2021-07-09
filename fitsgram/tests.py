4026bbba1156a4663fa960e8db41482d96c6705b
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-09 14:49:19 +0300

    Create the ImageTestClass that tests the image class

--- fitsgram/tests.py
+++ fitsgram/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image,Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.profile = Profile(user = self.user)
+        self.profile.save()
+        self.image = Image(id=1,image = 'path/to/image',image_name='test',image_caption='test caption',user=self.user,profile=self.profile)
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
