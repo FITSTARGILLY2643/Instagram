e41b376b5a193f79fdd06f6958f69f06322befff
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-09 12:42:10 +0300

    Create model image

--- fitsgram/models.py
+++ fitsgram/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Profile(models.Model):
-    profile_photo = models.CloudinaryField('image',blank = True)
+    profile_photo=cloudinary.models.CloudinaryField('image',blank = True)
     bio = models.TextField(max_length = 100,blank = True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     followers = models.ManyToManyField(User,blank=True,related_name='followers')
@@ -24,3 +24,12 @@ class Profile(models.Model):
     def search(cls,username):
         user = User.objects.get(username = username)
         return user
+
+class Image(models.Model):
+    image=cloudinary.models.CloudinaryField('image')
+    image_name = models.CharField(max_length = 30)
+    image_caption = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True,related_name='post_likes')
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
