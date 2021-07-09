4e9bad3fd010ca5fa2e6ab47df062719407e6e99
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-09 13:30:01 +0300

    Create the profile view function that renders the profile page

--- fitsgram/views.py
+++ fitsgram/views.py
@@ -18,3 +18,26 @@ def index(request):
         comments = Comment.objects.all()
         form = CommentForm()
         return render(request,"index.html",{"images":images,"form":form,"comments":comments})
+
+@login_required(login_url='accounts/login/')
+def profile(request,username):
+    current_user = request.user
+    try:
+        user = User.objects.get(username = username)
+        profile = Profile.objects.get(user = user)
+        images = Image.objects.filter(profile = profile)
+        following = Profile.objects.filter(followers = user).count()
+
+    except ObjectDoesNotExist:
+        return redirect('edit_profile',current_user)
+
+    if request.method == 'POST':
+        form = NewImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.profile = profile
+            image.save()
+    else:
+        form = NewImageForm()
+    return render(request,"profile.html",{"profile":profile, "images":images, "form":form,"following":following})
